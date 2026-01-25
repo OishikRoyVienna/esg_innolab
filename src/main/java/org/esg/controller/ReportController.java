b7773a3244cc62d3f0b604db300dbcb9f6fcabe7
OishikRoyVienna <oishik50@gmail.com>
2026-01-25 02:20:59 +0100

    bug fixes

--- src/main/java/org/esg/controller/ReportController.java
+++ src/main/java/org/esg/controller/ReportController.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.*;
 import java.math.BigDecimal;
 import java.util.Map;
 import java.util.stream.Collectors;
-
+//
 @RestController
 @RequestMapping("/api/esg")
 public class ReportController {
